739b6bb799367e7c107283621a12cc3cd03b667d
Eric Sheen <eric.sprog@gmail.com>
2024-07-02 03:21:29 -0700

    llm planner data curator

---
+++ scripts/caption_to_objects.py
@@ -0,0 +1,19 @@
+"""
+script that goes through combinations.json and gets rewritten caption and object descriptions
+stores as following:
+
+"data": [
+    {
+        caption: "A red car in a parking lot",
+        objects: [
+            "A red car in a parking lot"
+        ]
+    },
+    {
+        caption: "A red car in a parking lot",
+        objects: [
+            "A red car in a parking lot"
+        ]
+    }
+]
+"""
